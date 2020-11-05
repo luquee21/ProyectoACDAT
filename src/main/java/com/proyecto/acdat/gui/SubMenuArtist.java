dad89f8391ed6fe504d3c620915d95fcabcefed4
luque <miguelangelluque21@gmail.com>
2020-11-05 10:44:52 +0100

    -A la espera de terminar submenuartist

---
+++ src/main/java/com/proyecto/acdat/gui/SubMenuArtist.java
@@ -0,0 +1,48 @@
+package com.proyecto.acdat.gui;
+
+import com.proyecto.acdat.utils.Utilities;
+
+public class SubMenuArtist {
+
+    public static void artist() {
+        int option = 0;
+        do {
+            Utilities.P("---- Artista ----");
+            Utilities.P("1) Añadir artista");
+            Utilities.P("2) Borrar artista");
+            Utilities.P("3) Listar artista");
+            Utilities.P("4) Actualizar artista");
+            Utilities.P("5) Volver atrás");
+            option = Utilities.getInt();
+
+            switch (option) {
+                case 1:
+                    addArtist();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    Menu.start();
+                    break;
+                default:
+                    Utilities.P("Por favor, introduce una opción válida");
+                    break;
+            }
+
+        } while (option != 5);
+    }
+
+    public static void addArtist() {
+        String name = Utilities.getString("Introduce el nombre del artista");
+        String
+    }
+
+}
+
