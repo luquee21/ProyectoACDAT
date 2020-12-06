616f3871ccf53c315b14bbaaa198b1e31b9d87bb
Alvaro Barba <alvarobaru1997@gmail.com>
2020-12-06 18:29:10 +0100

    playlist funcionando

--- src/main/java/com/proyecto/acdat/App.java
+++ src/main/java/com/proyecto/acdat/App.java
@@ -2,6 +2,8 @@ package com.proyecto.acdat;
 
 import com.proyecto.acdat.model.Artist;
 import com.proyecto.acdat.model.ArtistDAO;
+import com.proyecto.acdat.model.PlayList;
+import com.proyecto.acdat.model.PlayListDAO;
 import com.proyecto.acdat.utils.Connection;
 
 import javax.persistence.EntityManager;
@@ -10,14 +12,6 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        EntityManager manager = Connection.getManager();
-        manager.getTransaction().begin();
-        ArtistDAO dao = new ArtistDAO();
-        Artist artist = dao.getArtist("ER KILLO");
-        dao.updateArtist(artist);
-        List<Artist> artists = dao.getAllArtists();
-        manager.getTransaction().commit();
-        System.out.println(artists);
 
 
     }
--- src/main/java/com/proyecto/acdat/model/PlayListDAO.java
+++ src/main/java/com/proyecto/acdat/model/PlayListDAO.java
@@ -1,23 +1,146 @@
 package com.proyecto.acdat.model;
 
+import com.proyecto.acdat.utils.Connection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class PlayListDAO extends PlayList {
 
-    private static final String SELECTALL = "SELECT * FROM Lista";
-    private static final String SELECTBYNAME = "SELECT * FROM Lista WHERE nombre=?";
-    private static final String SELECTBYEMAIL = "SELECT * FROM Lista WHERE id_usuario=?";
-    private static final String INSERTPLAYLIST = "INSERT INTO Lista (nombre,descripcion,id_usuario) VALUES(?,?,?)";
-    private static final String DELETEPLAYLIST = "DELETE FROM Lista WHERE id=?";
-    private static final String UPDATEPLAYLIST = "UPDATE Lista SET nombre=?, descripcion=? WHERE id=?";
-    private static final String INSERTSUB = "INSERT INTO Suscripcion values(?,?)";
-    private static final String DELETESUB = "DELETE FROM Suscripcion WHERE id_usuario=?";
-    private static final String SELECTSUB = "SELECT * FROM Suscripcion INNER JOIN Usuario ON Suscripcion.id_usuario=Usuario.id WHERE Suscripcion.id_lista=?";
-    private static final String INSERTSONG = "INSERT INTO Lista_cancion values(?,?)";
-    private static final String DELETESONG = "DELETE FROM Lista_cancion WHERE id_cancion=? AND id_lista=?";
-    private static final String SELECTBYID = "SELECT * FROM Lista WHERE id=?";
-
+    private EntityManager manager;
     public PlayListDAO(String name, String description) {
         super(name, description);
     }
+
+    public PlayListDAO() {
+    }
+
+    public void addplaylist(PlayList a) {
+        manager = Connection.getManager();
+        try {
+            manager.getTransaction().begin();
+            manager.persist(a);
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+    }
+
+    public void deletePlaylist(PlayList playList) {
+        manager = Connection.getManager();
+        try {
+            manager.getTransaction().begin();
+            PlayList a = manager.find(PlayList.class, playList.id);
+            manager.remove(a);
+            manager.getTransaction().commit();
+
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+    }
+
+    public void updatePlaylist(PlayList playList) {
+        manager = Connection.getManager();
+        try {
+            manager.getTransaction().begin();
+            PlayList a = manager.find(PlayList.class, playList.id);
+            a.setName(playList.getName());
+            a.setDescription(playList.getDescription());
+            manager.merge(a);
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+    }
+
+    public List<PlayList> getAllPlaylist() {
+        List<PlayList> playLists = null;
+        manager = Connection.getManager();
+        try {
+            manager.getTransaction().begin();
+            TypedQuery query = manager.createNamedQuery("Playlist.selectAll", PlayList.class);
+            playLists = (List<PlayList>) query.getResultList();
+            manager.getTransaction().commit();
+
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+        return playLists;
+    }
+
+    public List<PlayList> getPlaylistByName(String name) {
+        List<PlayList> playLists = null;
+        manager = Connection.getManager();
+        try {
+            manager.getTransaction().begin();
+            TypedQuery query = manager.createNamedQuery("Playlist.selectByName", PlayList.class);
+            query.setParameter("name", name);
+            playLists = (List<PlayList>) query.getResultList();
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+        return playLists;
+    }
+
+    public PlayList getPlaylistById(int id) {
+        PlayList playList = null;
+        manager = Connection.getManager();
+        try {
+            manager.getTransaction().begin();
+            TypedQuery query = manager.createNamedQuery("Playlist.selectById", PlayList.class);
+            query.setParameter("id", id);
+            playList = (PlayList) query.getSingleResult();
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+        return playList;
+    }
+
+    public List<PlayList> getPlaylistByUser(User user) {
+        List<PlayList> playLists = null;
+        manager = Connection.getManager();
+        try {
+            manager.getTransaction().begin();
+            TypedQuery query = manager.createNamedQuery("Playlist.selectByIdUser", PlayList.class);
+            query.setParameter("id_user", user.id);
+            playLists = (List<PlayList>) query.getResultList();
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+        return playLists;
+    }
+
+    public List<PlayList> getPlaylistBySub(int id) {
+        List<PlayList> playLists = null;
+        manager = Connection.getManager();
+        try {
+            manager.getTransaction().begin();
+            Query query = manager.createNativeQuery("Playlist.selectBySub", PlayList.class);
+            query.setParameter("id_playlist", id);
+            playLists = (List<PlayList>) query.getResultList();
+            manager.getTransaction().commit();
+        } catch (Exception e) {
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+        return playLists;
+    }
+
+
+
+
 
 
     //funciones del sub
